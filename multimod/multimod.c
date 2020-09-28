4f6142fc65aa999cbe2eb9127db275a733cd5f94
tracer-nju <tracer@nju.edu.cn>
2020-09-28 23:49:40 +0800

    Static hostname: asassin-Lenovo-Yoga-C940-14IIL          Icon name: computer-convertible            Chassis: convertible         Machine ID: c862bcd451424239aef141c9007bd8be            Boot ID: 28a3774786474ccd86deb8b5c0da192d   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-42-generic       Architecture: x86-64  23:49:40 up 13 min,  1 user,  load average: 0.92, 0.99, 0.80

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,25 @@
 #include <stdint.h>
 
+#define MOD_ADD_U(x, y, m) ((x)+ (y) < (x)) ? (((x+y+1)%(m) + (~0UL)%(m))%(m)) : (((x)+(y))%(m)) 
+
+
+static void cal_mpow_2(uint64_t b, uint64_t m, uint64_t b_pow_2[64]){
+    b_pow_2[0] = b;
+    for(int i = 1; i < 64; i++){
+        b_pow_2[i] = MOD_ADD_U(b_pow_2[i-1], b_pow_2[i-1], m);
+    }   
+}
+
+
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  uint64_t result = 0; 
+    uint64_t b_pow_2[64];
+
+    cal_mpow_2(b, m, b_pow_2);
+
+    for(int i = 0; i < 64; i++){
+        uint8_t flag = (a >> i) & 1;
+        result = MOD_ADD_U(result, flag*b_pow_2[i], m);
+    }
+    return result;
 }
