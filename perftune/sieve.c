ff6c2f008b127dfcb85a48e0a68b16c6b1745bad
tracer-nju <tracer@nju.edu.cn>
2021-01-04 21:33:16 +0800

    Static hostname: asassin-Lenovo-Yoga-C940-14IIL          Icon name: computer-convertible            Chassis: convertible         Machine ID: c862bcd451424239aef141c9007bd8be            Boot ID: 5300b58dd5d7462cb7e2edfec8322a59   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  21:33:16 up  5:02,  1 user,  load average: 0.68, 0.44, 0.30

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -5,25 +5,22 @@
 
 #define N 10000000
 
-static bool is_prime[N];
+static bool vis[N];
 static int  primes[N];
 
 int *sieve(int n) {
   assert(n + 1 < N);
-  for (int i = 0; i <= n; i++)
-    is_prime[i] = true;
+  memset(vis, 0, sizeof(vis));
+  cnt = 0;
 
   for (int i = 2; i <= n; i++) {
-    for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
+    if(!vis[i]) primes[cnt++] = i;
+    for(int j = 0; j < cnt && 1ll * i * primes[j] < n; j++)
+    {
+      vis[i*primes[j]] = 1;
+      if(i % primes[j] == 0) break;
     }
   }
 
-  int *p = primes;
-  for (int i = 2; i <= n; i++)
-    if (is_prime[i]) {
-      *p++ = i;
-    }
-  *p = 0;
   return primes;
 }
