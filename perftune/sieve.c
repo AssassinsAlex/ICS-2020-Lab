f3d6c993d3aa20d7034a152be7f3452a7873915a
tracer-nju <tracer@nju.edu.cn>
2021-01-05 17:27:20 +0800

    Static hostname: asassin-Lenovo-Yoga-C940-14IIL          Icon name: computer-convertible            Chassis: convertible         Machine ID: c862bcd451424239aef141c9007bd8be            Boot ID: 3240b53f9fba43e4959d195640b60af7   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  17:27:20 up 16 min,  1 user,  load average: 0.69, 0.46, 0.37

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -12,8 +12,10 @@ int *sieve(int n) {
   assert(n + 1 < N);
   //memset(vis, 0, sizeof(vis));
   int cnt = 0;
+  //primes[cnt++] = 2;
+  //primes[cnt++] = 3;
   for (int i = 2; i <= n; i++) {
-    if(i%2 == 0 || i%7 == 0) continue;
+    if(i%2 == 0 || i%3 == 0) continue;
     if(!vis[i]) primes[cnt++] = i;
     for(int j = 0; j < cnt && i * primes[j] <= n; j++)
     { 
