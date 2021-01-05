d0c1f92a95046b9f64ec4addbb8dcbb2486f9be1
tracer-nju <tracer@nju.edu.cn>
2021-01-05 17:27:51 +0800

    Static hostname: asassin-Lenovo-Yoga-C940-14IIL          Icon name: computer-convertible            Chassis: convertible         Machine ID: c862bcd451424239aef141c9007bd8be            Boot ID: 3240b53f9fba43e4959d195640b60af7   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  17:27:51 up 17 min,  1 user,  load average: 0.69, 0.48, 0.38

--- perftune/main.c
+++ perftune/main.c
@@ -4,7 +4,7 @@
 int *sieve(int n);
 
 int main() {
-  int *primes = sieve(10000000-2);
+  int *primes = sieve(400);
   
   for (int *cur = primes, i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
