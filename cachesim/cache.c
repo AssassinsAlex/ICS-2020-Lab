3bbd85cea590dae4452b615b894b84977c8da2d6
tracer-nju <tracer@nju.edu.cn>
2021-01-07 20:44:48 +0800

    Static hostname: asassin-Lenovo-Yoga-C940-14IIL          Icon name: computer-convertible            Chassis: convertible         Machine ID: c862bcd451424239aef141c9007bd8be            Boot ID: 3fa4c3d212674802afa2c187c065c9c3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  20:44:48 up 1 day, 49 min,  1 user,  load average: 0.72, 0.45, 0.33

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -8,16 +8,82 @@ static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
 
+typedef struct _cache_block
+{
+  bool valid;
+  bool dirty_bit;
+  uint32_t tag;
+  uint8_t data[BLOCK_SIZE];
+} cache_block;
+
+static cache_block* cache;
+static uint32_t total_num;
+static uint32_t group_num;
+static uint32_t row_num;
+
 // TODO: implement the following functions
 
+/* 按照数据对起处理 */
+
+static bool check_hit(uintptr_t addr, uint32_t *row_addr){
+  addr /= BLOCK_SIZE;
+  uint32_t block_addr = addr % group_num;
+  uint32_t dst_tag = addr / group_num;
+  cache_block* cur = cache + block_addr * row_num;
+  for(uint32_t i = 0; i < row_num; i++){
+    if((cur+i)->valid && (cur+i)->tag == dst_tag){
+      *row_addr = i;
+      return true;
+    }
+  }
+  *row_addr = 0;
+  return false;
+}
+
+static void allocate(uintptr_t addr, uint32_t *row_addr){
+  addr /= BLOCK_SIZE;
+  uint32_t block_addr = addr % group_num;
+  uint32_t dst_tag = addr / group_num;
+  uint32_t ram_row = rand() % row_num;
+  cache_block* cur = cache + block_addr * row_num + ram_row;
+  if(cur->valid && cur->dirty_bit){
+    mem_write(cur->tag * block_addr, &(cur->dirty_bit));
+  }
+  mem_read(cur->tag * block_addr, &(cur->data));
+  cur->valid = true;
+  cur->dirty_bit = false;
+  cur->tag = dst_tag;
+}
+
 uint32_t cache_read(uintptr_t addr) {
-  return 0;
+  uint32_t row_addr = 0;
+  if(check_hit(addr, &row_addr)){
+    allocate(addr, &row_addr);
+  }
+  cache_block *cur =  cache + addr / BLOCK_SIZE % group_num + row_addr;
+  uint32_t *p =(void *)(cur->data)+ (addr & ~0x3) % BLOCK_SIZE;
+  return *p;
 }
 
 void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
+  uint32_t row_addr = 0;
+  if(check_hit(addr, &row_addr)){
+    allocate(addr, &row_addr);
+  }
+  cache_block *cur =  cache + addr / BLOCK_SIZE % group_num + row_addr;
+  uint32_t *p =(void *)(cur->data)+ (addr & ~0x3) % BLOCK_SIZE;
+  *p = (*p & ~wmask) | (data & wmask);
+  cur->dirty_bit = true;
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+  srand(time(NULL));
+  row_num = associativity_width;
+  total_num = total_size_width / BLOCK_SIZE;
+  group_num = total_num / row_num;
+  cache = (cache_block *) malloc(sizeof(cache_block)*total_num);
+  memset(cache, 0, total_num);
+
 }
 
 void display_statistic(void) {
