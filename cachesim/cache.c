ca0b45def826115ad06c8971cf815b28dd40b6cf
tracer-nju <tracer@nju.edu.cn>
2021-01-07 22:00:24 +0800

    Static hostname: asassin-Lenovo-Yoga-C940-14IIL          Icon name: computer-convertible            Chassis: convertible         Machine ID: c862bcd451424239aef141c9007bd8be            Boot ID: 3fa4c3d212674802afa2c187c065c9c3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.4.0-52-generic       Architecture: x86-64  22:00:24 up 1 day,  2:04,  1 user,  load average: 0.44, 0.30, 0.27

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -49,7 +49,7 @@ static void allocate(uintptr_t addr, uint32_t *row_addr){
   uint32_t ram_row = rand() % row_num;
   cache_block* cur = cache + block_addr * row_num + ram_row;
   if(cur->valid && cur->dirty_bit){
-    mem_write(cur->tag * group_num+ block_addr, (uint8_t *)&(cur->data));
+    mem_write(cur->tag * group_num + block_addr, (uint8_t *)&(cur->data));
   }
   mem_read(dst_tag * block_addr, (uint8_t *)&(cur->data));
   cur->valid = true;
